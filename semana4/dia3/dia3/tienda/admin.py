e34d44f46803a3562e7d1b55ab47c550cd37f5bb
Fabio Menacho Landa <fmenacho@uni.pe>
2021-08-14 13:28:19 -0500

    Django e-commerce basico

---
+++ semana4/dia3/dia3/tienda/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Categoria, Producto
+
+admin.site.register(Categoria)
+# admin.site.register(Producto)
+# decorador https://docs.djangoproject.com/en/3.2/ref/contrib/admin/
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    # establezco q quiero q se vea
+    list_display = ('pk','nombre','categoria','precio','stock')
+    # para clickear y editar
+    list_display_links = ('pk','nombre')
+    list_editable = ('categoria','precio','stock')
+    search_fields = ['nombre']
